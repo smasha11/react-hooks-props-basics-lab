cd9d32fed265c9a32a223e6841ea49ce4f1ee653
smasha11 <ismail.hussein.1120@gmail.com>
2024-06-29 08:44:29 +0300

    lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,24 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
 
-function About() {
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {props.bio && <p>{props.bio}</p>}
+            <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+       <Links /> 
+
+
     </div>
   );
+
+
+
+
+
 }
 
 export default About;
